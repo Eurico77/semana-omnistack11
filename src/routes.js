8e37f7dde431e9ef9c9b43b4d220e104af3a3c4f
Eurico77 <euricomagalhaes61@gmail.com>
2020-03-24 14:58:13 -0300

    ok

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ const routes = express.Router();
 
 routes.get("/ongs", async (req, res)=>{
   const ongs = await connection('ongs').select('*')
+
+  return res.json(ongs);
 })
 
 routes.post("/ongs", async (req, res) => {
